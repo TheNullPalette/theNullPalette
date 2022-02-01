391358011ce413c6b01ee0f61fc0ef500c1b1643
Naman Agarwal <naman.agarwal2019@vitstudent.ac.in>
2022-02-01 11:40:49 +0530

    feat: :zap: add brand animation in nav bar

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,11 +1,37 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import "./index.css"
 
 export default function NavBar() {
+  let oldScrollY = 0
+
+  const [brandAnimate, setBrandAnimate] = useState(false)
+
+  const controlDirection = () => {
+    if (window.scrollY > oldScrollY) {
+      setBrandAnimate(true)
+    } else {
+      setBrandAnimate(false)
+    }
+    oldScrollY = window.scrollY
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", controlDirection)
+    return () => {
+      window.removeEventListener("scroll", controlDirection)
+    }
+  }, [])
+
   return (
     <div className="navbar-root-container">
       <div className="logo-container">
-        <p>Null Palette</p>
+        <p>Null Palette Logo</p>
+      </div>
+      <div className="brand-container">
+        <span data-active={!brandAnimate}>the</span>
+        <span>Null</span>
+        <span data-active={!brandAnimate}>Palette</span>
+        <span>.</span>
       </div>
       <div className="navigation-buttons-container">
         <div className="navigationButton">
