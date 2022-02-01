503919a452d67222e56ea39246e1851c3cfedec9
Naman Agarwal <naman.agarwal2019@vitstudent.ac.in>
2022-02-01 11:41:16 +0530

    feat: :rocket: add cards and animation in testimonials

--- src/components/Testimonials/index.js
+++ src/components/Testimonials/index.js
@@ -1,14 +1,36 @@
-import React from 'react'
+import React from "react"
+import "./index.css"
 
-export default function Testimonials(){
-    return(
-        <div style={{backgroundColor: 'blue', padding: '1%'}}>
-            <h1>Testimonials</h1>
-            <div>
-                <p>XYZ Person</p>
-                <p>ABCD Review</p>
-            </div>
-            {/* scrollable container containing the Testimonials */}
-        </div>
-    )
+const Card = () => {
+  return (
+    <div className="flex shrink-0 w-[350px] h-[200px] bg-white rounded-lg drop-shadow-lg">
+      Card
+    </div>
+  )
+}
+
+export default function Testimonials() {
+  const totalCards = 12
+
+  return (
+    <div style={{ backgroundColor: "pink", padding: "5%" }}>
+      <h1
+        style={{ fontFamily: "Lobster, serif" }}
+        className="text-5xl text-center mb-10"
+      >
+        Testimonials
+      </h1>
+      {/* scrollable container containing the Testimonials */}
+      <div
+        className="flex gap-[20px] overflow-hidden sliding-window"
+        data-children="5"
+      >
+        {Array(12)
+          .fill(0)
+          .map((x, i) => {
+            return <Card key={i} />
+          })}
+      </div>
+    </div>
+  )
 }
