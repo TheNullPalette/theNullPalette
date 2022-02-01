39a34db7ce68642358c205efbdeb2137aa967f0f
Naman Agarwal <naman.agarwal2019@vitstudent.ac.in>
2022-02-01 11:40:20 +0530

    feat: :fire: add tailwind css

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,6 +31,16 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `Lobster\:300,400,700`,
+        ],
+        display: 'swap'
+      }
+    },
+    'gatsby-plugin-postcss'
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
